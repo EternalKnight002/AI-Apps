272d7bff3878ed34facabe8bd05eb2e209e5348e
EternalKnight002 <resoamankumar@gmail.com>
2026-01-13 18:02:08 +0530

    basic styling done

---
+++ src/app/ui/completion/page.tsx
@@ -0,0 +1,16 @@
+"use client" ;
+
+export default function CompletionPage() {
+    return (
+        <div>
+            <form action="">
+                <div>
+                    <input style={{ width: '400px' , backgroundColor: 'lightgray' , color: 'black' }}
+                    type="text" placeholder="How can i help you ?" />
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
